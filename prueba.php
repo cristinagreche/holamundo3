eadf25ebb8a2a9a3d82bd55fdaa47f19ba0ad7b6
NotFerCam <davidfercam69@gmail.com>
2020-09-22 20:25:51 +0200

    commit1aes13

---
+++ prueba.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>prueb</title>
+</head>
+<body>
+    <?php 
+    $a=13;
+    $b=("9,99");
+    $c="ejemplo variables php";
+    //ipresion
+    echo $a."<br>",$b."<br>".$c;
+    $mivariable="pureba git merge";
+    $otra = 36;
+    ?>
+
+</body>
+</html>
